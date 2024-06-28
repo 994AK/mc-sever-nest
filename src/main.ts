52257d660c2baef8eef4106a663f128e8a9b4438
994AK <1505398065@qq.com>
2024-06-29 02:23:40 +0800

    fix: 修复跨域问题

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const configService = app.get(ConfigService);
-  const databaseUrl = configService.get<string>('DATABASE_URL');
-  console.log(`Connecting to database: ${databaseUrl}`);
+  // 配置CORS选项
+  app.enableCors({
+    origin: '*', // 允许所有来源
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // 允许所有HTTP方法
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  });
 
   await app.listen(3000);
 }
